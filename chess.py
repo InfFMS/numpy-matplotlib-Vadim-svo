ef1c7e6def2a3d91f98a9316bca39e31330d453f
Дягилев Вадим Георгиевич <v.dyagilev@main.nsu.ru>
2025-02-08 14:20:56 +0700

    сделал 1, почти сделал 2

--- chess.py
+++ chess.py
@@ -8,7 +8,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #создаём доску
-x = np.array([[10,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,10],[10,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,10],[10,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,10],[10,0,10,0,10,0,10,0], [0,10,0,10,0,10,0,10]])
+bruh, ferz = plt.subplots()
+x = np.array([[1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0], [0,1,0,1,0,1,0,1]])
+plt.imshow(x, cmap = 'hot', interpolation='nearest')
 col_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 row_labels = range(1, 9)
 plt.xticks(range(8), col_labels)
@@ -18,13 +20,29 @@ plt.yticks(range(8), row_labels)
 X_ferz = int(input())
 Y_ferz = int(input())
 
+
+
 #считаем где он и куда может атаковать
 for i in range(8):
-    x[Y_ferz][i] = 6
-    x[i][X_ferz] = 6
-    x[abs(Y_ferz-i)][abs(X_ferz-i)] = 6
-x[Y_ferz][X_ferz] = 3
+    #x[Y_ferz][i] = 6
+    ferzat = plt.Circle((int(Y_ferz),int(i)), 0.314, facecolor='pink')
+    ferz.add_patch(ferzat)
+    #x[i][X_ferz] = 6
+    ferzat = plt.Circle((int(i), int(X_ferz)), 0.314, facecolor='pink')
+    ferz.add_patch(ferzat)
+    #x[abs(Y_ferz-i)][abs(X_ferz-i)] = 6
+    ferzat = plt.Circle((int(Y_ferz-i), int(X_ferz-i)), 0.314, facecolor='pink')
+    ferz.add_patch(ferzat)
+    ferzat = plt.Circle((int(Y_ferz+i), int(X_ferz+i)), 0.314, facecolor='pink')
+    ferz.add_patch(ferzat)
+    ferzat = plt.Circle((int(Y_ferz - i), int(X_ferz + i)), 0.314, facecolor='pink')
+    ferz.add_patch(ferzat)
+    ferzat = plt.Circle((int(Y_ferz + i), int(X_ferz - i)), 0.314, facecolor='pink')
+    ferz.add_patch(ferzat)
+
+ferzat = plt.Circle((int(Y_ferz), int(X_ferz)), 0.414, facecolor='brown')
+ferz.add_patch(ferzat)
 
 
-plt.imshow(x, cmap = 'hot', interpolation='nearest')
+
 plt.show()
--- kubick.py
+++ kubick.py
@@ -5,3 +5,53 @@
 # Вероятность выпадения каждого значения.
 # Максимальное количество подряд выпавших одинаковых значений.
 # Визуализируйте результаты в виде гистограммы.
+
+import random as ran
+import matplotlib as plt
+import numpy as np
+
+#делаем список
+x = []
+count = [0, 0, 0, 0, 0, 0]
+for i in range (10):
+    x.append(ran.randint(1, 6))
+
+#сортируем списко
+y = x.copy()
+y.sort()
+
+#считаем значения
+c = 0
+b = 1
+for i in range(10):
+    if y[i] == b:
+        count[c] += 1
+    else:
+        count[y[i]-1] += 1
+        b += 1
+        c += 1
+
+#считаем вероятнлости
+ver = []
+for i in range(6):
+    ver.append(count[i]/1000)
+
+#считаем макс серию
+max1 = x[1]
+max2 = 1
+p=[]
+for i in range(10):
+    if x[i]==max1:
+        max2 += 1
+        p.append(max2)
+    else:
+        max2=1
+
+
+
+print(x)
+print(y)
+print(count)
+print(ver)
+print(max(p))
+
